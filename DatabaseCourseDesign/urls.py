c3770db348e33a25dde8722b0cad981aac5214e0
stone <1453793807@qq.com>
2018-09-22 15:03:58 +0800

    'managerinit'

--- DatabaseCourseDesign/urls.py
+++ DatabaseCourseDesign/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,re_path,include
 from django.views.generic.base import TemplateView
-from xxadmin import xadmin
+from extra_apps import xadmin
 from Hikarian.views import hasLoggedIn
 urlpatterns = [
     path('admin/', admin.site.urls),
